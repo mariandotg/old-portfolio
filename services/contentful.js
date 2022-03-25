686c89743b279353480de35de53281cd0d518db4
mariandotg <marianguillaume.m@gmail.com>
2022-03-24 21:39:54 -0300

    Changed file extension convention for services

---
+++ services/contentful.js
@@ -0,0 +1,11 @@
+export const getContentfulData = () => {
+  const contentful = require('contentful');
+  const client = contentful.createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  });
+  return client
+    .getEntries()
+    .then((response: any) => response.items)
+    .catch((error: any) => console.log(error));
+};
