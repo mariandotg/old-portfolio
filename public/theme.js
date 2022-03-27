7cd4c7304b01e01720871e8728589e602b99ffa4
mariandotg <marianguillaume.m@gmail.com>
2022-03-27 12:46:28 -0300

    Fixed flash bug with a custom script

---
+++ public/theme.js
@@ -0,0 +1,8 @@
+(function initTheme() {
+  const theme = localStorage.getItem('theme')
+    ? localStorage.getItem('theme')
+    : 'dark';
+  if (theme === 'dark') {
+    document.querySelector('html').classList.add('dark');
+  }
+})();
