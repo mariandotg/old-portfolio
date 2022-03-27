ca9c6e0bb9b83dbfe1b703811d72617b7bcdac7e
mariandotg <marianguillaume.m@gmail.com>
2022-03-27 12:45:18 -0300

    Added Dark and Light Theme switcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,47 @@
+import { useEffect, useState } from 'react';
 import type { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
 import { getContentfulData } from '../services/contentful';
 import contentfulDataAdapter from '../adapters/contentfulDataAdapter';
 import { Data } from '../models/data';
 import About from '../components/About';
-import Head from 'next/head';
 
 interface Props {
   data: Data;
 }
 
 const Home: NextPage<Props> = ({ data }) => {
+  const [theme, setTheme] = useState(
+    typeof window !== 'undefined' ? localStorage.theme : 'dark'
+  );
+  const colorTheme = theme === 'dark' ? 'light' : 'dark';
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', theme);
+    }
+  }, [theme, colorTheme]);
+
   return (
     <>
       <Head>
         <link rel='icon' href='./favicon.ico' />
       </Head>
       <div className='p-8'>
+        {colorTheme === 'light' ? (
+          <button className='bg-white p-4' onClick={() => setTheme('light')}>
+            change to light
+          </button>
+        ) : (
+          <button className='bg-white p-4' onClick={() => setTheme('dark')}>
+            change to dark
+          </button>
+        )}
         <About data={data.about} />
       </div>
     </>
