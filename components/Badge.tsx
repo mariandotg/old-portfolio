a6af9ea6e4c1a7c05799b7d216f5d47a115c4d7b
mariandotg <marianguillaume.m@gmail.com>
2022-04-01 12:18:09 -0300

    Refactor of Badge component

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,22 +1,12 @@
-import SvgIcon from './SvgIcon';
-import { Tech } from '../models/data';
-
 interface Props {
-  tech: Tech;
+  title: string;
 }
 
-const Badge = ({ tech }: Props) => {
+const Badge = ({ title }: Props) => {
   return (
-    <li className='flex items-center gap-4 text-xs font-bold'>
-      <SvgIcon
-        path={tech.fields.iconPath}
-        classes='hidden mobile:flex h-6 w-6 text-card-light-text dark:text-card-dark-text'
-        themeSensitive
-      />
-      <p className='px-2 py-1 bg-badge-light text-card-light-text rounded-large h-fit'>
-        {tech.fields.title.toUpperCase()}
-      </p>
-    </li>
+    <span className='px-2 py-1 text-xs font-bold bg-badge-light text-card-light-text rounded-large h-fit'>
+      {title.toUpperCase()}
+    </span>
   );
 };
 
